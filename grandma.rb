0c6a485b622345d16f69adf5a4b69cb3880d97b0
Keith Filkins <kafilkins@me.com>
2020-11-30 22:35:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+    
 
   end
   end
