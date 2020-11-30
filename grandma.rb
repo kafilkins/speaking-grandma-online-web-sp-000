5dba8f5583713dd65228ea6ffec647e78a73f689
Keith Filkins <kafilkins@me.com>
2020-11-30 22:28:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma
   if "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
   end
 # Whatever you say to grandma, she should respond with
